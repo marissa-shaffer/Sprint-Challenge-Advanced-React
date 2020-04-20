c5e5d30b9ed315c40e700276c911454f592835c7
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-04-19 19:17:12 -0500

    Set constructor & axios call in componentDidMount

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      players: []
+    }; 
+  }
+
+  componentDidMount(){
+    axios 
+    .get("http://localhost:5000/api/players")
+    .then(res => {
+      this.setState({
+        players: res.data
+      });
+    })
+    .catch(err => console.log("no players", err));
+    console.log("componentDidMount running")
+  }
+
+  render(){
+    return (
+      <div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
