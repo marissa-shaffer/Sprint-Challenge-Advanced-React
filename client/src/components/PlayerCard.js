a3daa53535fbcde7efb0dac4e5e957c05600d03a
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-04-19 19:31:47 -0500

    Set up playerCard component

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -3,7 +3,9 @@ import React from 'react';
 function PlayerCard (props) {
     return (
        <div>
-           
+           <h2>{props.player.name}</h2>
+           <p>Country: {props.player.country}</p>
+           <p>Searches: {props.player.searches}</p>
        </div> 
     )
 }
